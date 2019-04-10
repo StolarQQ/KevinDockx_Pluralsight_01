14f5de6f0b3f0025b48c909fee42356e0e344f00
Stolar <stolar28@gmail.com>
2019-04-11 01:15:00 +0200

    Added test data

---
+++ CityInfo/CityInfo.Api/CitiesDataStore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using CityInfo.Api.Models;
+
+namespace CityInfo.Api
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower.",
+                }
+            };
+        }
+    }
+}
