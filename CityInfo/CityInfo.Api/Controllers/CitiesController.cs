822f59a1d30eb1e5029c005d62398ab5b84c3540
Stolar <stolar28@gmail.com>
2019-04-11 01:16:09 +0200

    First CitiesController

---
+++ CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCityById(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == id);
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cityToReturn);
+        }
+    }
+}
