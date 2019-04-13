f73e6d2f0a2fe915ac4f0df485e74f3401b16f73
Stolar <stolar28@gmail.com>
2019-04-13 02:20:07 +0200

    Added PointsOfInterestController
    Added HttpGet methods, first for list of pointsofinterest for city, second getting specify points

---
+++ CityInfo/CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityid}/pointsofinterest")]
+        public IActionResult GetPointOfInterest(int cityid)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityid);
+            if (city == null)
+                return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityid}/pointsofinterest/{id}")]
+        public IActionResult GetPointOfInterest(int cityid, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(x => x.Id == cityid);
+            if (city == null)
+                return NotFound();
+
+            var specifyPointOfInterest = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+            if(specifyPointOfInterest == null)
+                return NotFound();
+
+            return Ok(specifyPointOfInterest);
+        }
+    }
+}
