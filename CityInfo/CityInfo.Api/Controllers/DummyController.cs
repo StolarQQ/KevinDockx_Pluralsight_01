740559be468efa2b7e99ac738b1d5701553b0bf8
Stolar <stolar28@gmail.com>
2019-04-15 23:52:15 +0200

    Test controller

---
+++ CityInfo/CityInfo.Api/Controllers/DummyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    public class DummyController : Controller
+    {
+        private CityInfoContext _cityInfoContext;
+
+        public DummyController(CityInfoContext cityInfoContext)
+        {
+            _cityInfoContext = cityInfoContext;
+        }
+
+        [HttpGet]
+        [Route("api/database")]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
